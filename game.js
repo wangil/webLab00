0b672f699ab8f0ec9cacff0c523c790c44ca129a
wangil <wangil93@naver.com>
2015-11-18 19:50:27 +0900

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,6 @@ var numberOfTarget = 3;
 var targetBlocks = [];
 var selectedBlocks = [];
 var timer;
-var cnt = 0;
 
 document.observe('dom:loaded', function(){
 	$("start").onclick = stopToStart;
@@ -14,8 +13,9 @@ document.observe('dom:loaded', function(){
 
 function stopToStart(){
 	$("state").update("Ready");
+	$("answer").update("0/0");
+	timer = 0;
 	reset();
-    stopGame();
     startToSetTarget();
 }
 
@@ -23,57 +23,91 @@ function stopGame(){
 	$("state").update("Stop");
 	$("answer").update("0/0");
 	reset();
+	timer = 1;
 }
 
 function startToSetTarget(){
-	for(var i=0;i<3;i++){
-		var ran = Math.floor(Math.random()*10);
-		while(ran == 9 || targetBlocks.indexOf(ran)!=-1){
-			ran = Math.floor(Math.random()*10);
+	reset();
+	if(timer != 1){
+		for(var i=0;i<3;i++){
+			var ran = Math.floor(Math.random()*10);
+			while(ran == 9 || targetBlocks.indexOf(ran)!=-1){
+				ran = Math.floor(Math.random()*10);
+			}
+			targetBlocks[i] = ran;
 		}
-		targetBlocks[i] = ran;
+		for(var i=0;i<3;i++){
+			console.log(targetBlocks[i]);
+		}
+		setTimeout(setTargetToShow,interval);
 	}
-	for(var i=0;i<3;i++){
-		console.log(targetBlocks[i]);
-	}
-	setTimeout(setTargetToShow,interval);
 }
 
 function setTargetToShow(){
-	$("state").update("Memorize!");
-	var block = $$(".block");
-	for(var i=0;i<targetBlocks.length;i++){
-		block[targetBlocks[i]].addClassName("target");
+	if(timer != 1){
+		$("state").update("Memorize!");
+		var block = $$(".block");
+		for(var i=0;i<numberOfTarget;i++){
+			block[targetBlocks[i]].addClassName("target");
+		}
+		
+		setTimeout(showToSelect,interval);
 	}
-	setTimeout(showToSelect,interval);
 }
 
 function showToSelect(){
-	$("state").update("Select!");
-	var block = $$(".block");
-	for(var i=0;i<targetBlocks.length;i++){
-		block[targetBlocks[i]].removeClassName("target");
-	}
-	for (var i in $$('.block')) {
-		$$('.block')[i].onclick = function (){
-			selectedBlocks.push(this.readAttribute('data-index'));
+	if(timer != 1){
+		$("state").update("Select!");
+		var block = $$(".block");
+		for(var i=0;i<numberOfTarget;i++){
+			block[targetBlocks[i]].removeClassName("target");
 		}
+		for (var i in $$('.block')) {
+			$$('.block')[i].onclick = function (){
+				selectedBlocks.push(this.readAttribute('data-index'));
+				if(selectedBlocks.slice(0,-1).indexOf(selectedBlocks[selectedBlocks.length-1])!=-1){
+					console.log("pop");
+					selectedBlocks.pop();
+				}
+				else if(selectedBlocks.length<numberOfTarget+1){
+					this.addClassName("selected");
+					console.log(this.readAttribute('data-index'));
+				}
+				
+			}
+		}
+		setTimeout(selectToResult,interval);
 	}
-	for(var i=0;i<selectedBlocks.length;i++){
-		block[selectedBlocks[i]].addClassName("selected");
-	}
-	setTimeout(selectToResult,interval);
 }
 
 function selectToResult(){
-	$("state").update("Checking");
-	var block = $$(".block");
-	for(var i=0;i<selectedBlocks.length;i++){
-		block[selectedBlocks[i]].removeClassName("selected");
+	if(timer != 1){
+		$("state").update("Checking");
+		var block = $$(".block");
+		var ans =0;
+		for(var i=0;i<numberOfTarget;i++){
+			block[selectedBlocks[i]].removeClassName("selected");
+			var a = parseInt(selectedBlocks[i]);
+			if(targetBlocks.indexOf(a) != -1){
+				ans += 1;
+			}
+		}
+		var data = document.getElementById("answer").innerHTML;
+		data = data.split("/");
+		data[0] = parseInt(data[0]) + ans;
+		data[1] = parseInt(data[1]) + numberOfTarget;
+		$("answer").update(data[0]+"/"+data[1]);
+		
+		setTimeout(startToSetTarget,interval);
 	}
 }
 function reset(){
 	targetBlocks = [];
 	selectedBlocks = [];
-	timer = 0;
+	var block = $$(".block");
+	for(var i=0;i<numberOfBlocks;i++){
+		if(block[i].hasClassName("target")){
+			block[i].removeClassName("target");
+		}
+	}
 }
